7d82c7c6e7d994247fa5e65e27bc8aeba4f2fc05
ClaytonMurray92 <clamurr@okstate.edu>
2024-10-20 16:46:27 -0400

    Add files via upload

---
+++ Week 2 Project 5.py
@@ -0,0 +1,4 @@
+mass = float(input("Enter object mass in kilograms: "))
+velocity = float(input("Enter object velocity in meters per second: "))
+momentum = mass * velocity
+print("Object momentum: ", momentum, "kg m/s")
